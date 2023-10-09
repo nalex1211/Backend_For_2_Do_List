0a7d3a725ed024a6339f4a157046c7efb3ca10dd
Alex <nalex1211@gmail.com>
2023-10-09 21:19:11 +0300

    AddedSomeTasks

--- Backend_For_2_Do_List/Helpers/TaskHub.cs
+++ Backend_For_2_Do_List/Helpers/TaskHub.cs
@@ -2,10 +2,50 @@
 
 namespace Backend_For_2_Do_List.Helpers;
 
+//public class TaskHub : Hub
+//{
+//    public async Task SendTasks(string tasks)
+//    {
+//        await Clients.All.SendAsync("ReceiveTasks", tasks);
+//    }
+//}
+
 public class TaskHub : Hub
 {
-    public async Task SendTasks(string tasks)
+    private static List<string> tasks = new List<string>();
+
+    public async Task SendTasks(string updatedTasks)
     {
-        await Clients.All.SendAsync("ReceiveTasks", tasks);
+        tasks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string>>(updatedTasks);
+        await Clients.All.SendAsync("ReceiveTasks", updatedTasks);
+    }
+
+    public async Task TaskAdded(string task)
+    {
+        tasks.Add(task);
+        await Clients.All.SendAsync("TaskAdded", task);
+    }
+
+    public async Task TaskEdited(string oldTask, string newTask)
+    {
+        int index = tasks.IndexOf(oldTask);
+        if (index != -1)
+        {
+            tasks[index] = newTask;
+            await Clients.All.SendAsync("TaskEdited", new { oldTask, newTask });
+        }
+    }
+
+    public async Task TaskDeleted(string task)
+    {
+        tasks.Remove(task);
+        await Clients.All.SendAsync("TaskDeleted", task);
+    }
+
+    public async Task TaskMoved(List<string> updatedTasks)
+    {
+        tasks = updatedTasks;
+        string tasksJson = Newtonsoft.Json.JsonConvert.SerializeObject(tasks);
+        await Clients.All.SendAsync("ReceiveTasks", tasksJson);
     }
 }
